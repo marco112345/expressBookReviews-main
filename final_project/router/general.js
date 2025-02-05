36006133e50220cc4463d4cb19bd5d2e4c833bb0
marco112345 <marco@firstfactory.com>
2025-02-05 15:55:00 -0600

    Task 3, books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,13 +21,29 @@ public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
   let bookSelected = books[isbn]
-  return res.status(300).json(bookSelected);
+  return res.status(200).json(bookSelected);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const authorName = req.params.author; // Get the author from the URL parameter
+  const bookKeys = Object.keys(books); // Get all the keys (book IDs)
+
+  const matchingBooks = []; // Array to store books by the specified author
+
+  for (const key of bookKeys) {
+    const book = books[key];
+    if (book.author === authorName) {
+      matchingBooks.push(book);
+    }
+  }
+  if (matchingBooks.length > 0) {
+     return res.status(200).json(matchingBooks); // Send the matching books as JSON
+  } else {
+    return res.status(404).json({ message: "No books found for that author." }); // Send 404 if no books found
+  }
+  
 });
 
 // Get all books based on title
